7b6cbcb884d1789f8adfe8358cc4ca778deb76ba
gifteddevsmd <babydave081@gmail.com>
2025-07-25 01:04:32 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -55,20 +55,20 @@ await MegaMdEmpire.sendMessage(MegaMdEmpire.user.id, {
 📁ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ᴄʀᴇᴅs.ᴊsᴏɴ ғɪʟᴇ ᴘʀᴏᴠɪᴅᴇᴅ ɪɴ ʏᴏᴜʀ sᴇssɪᴏɴ ғᴏʟᴅᴇʀ. 
 
 _*🪀sᴛᴀʏ ᴛᴜɴᴇᴅ ғᴏʟʟᴏᴡ ᴡʜᴀᴛsᴀᴘᴘ ᴄʜᴀɴɴᴇʟ:*_ 
-> _https://whatsapp.com/channel/0029Vb6covl05MUWlqZdHI2w_
+> _https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k_
 
 _*ʀᴇᴀᴄʜ ᴍᴇ ᴏɴ ᴍʏ  ᴛᴇʟᴇɢʀᴀᴍ:*_  
-> _t.me/LordMega0_
+> _t.me/Digladoo_
 
 
-> 🫩ʟᴀsᴛʟʏ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ᴏʀ ᴄʀᴇᴅs.ᴊsᴏɴ ғɪʟᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ʙʀᴏ ᴀɴᴅ ғᴏʀ ᴀɴʏ ʜᴇʟᴘ _*ᴅᴍ ᴏᴡɴᴇʀ https://wa.me/256753679393*_  `,
+> 🫩ʟᴀsᴛʟʏ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ᴏʀ ᴄʀᴇᴅs.ᴊsᴏɴ ғɪʟᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ʙʀᴏ ᴀɴᴅ ғᴏʀ ᴀɴʏ ʜᴇʟᴘ _*ᴅᴍ ᴏᴡɴᴇʀ https://wa.me/254104260236*_  `,
 
   contextInfo: {
     externalAdReply: {
       title: "Successfully Generated Session",
       body: "Mega-MD Session Generator 1",
       thumbnailUrl: "https://cdn.ironman.my.id/i/ttlm6m.jpg",
-      sourceUrl: "https://whatsapp.com/channel/0029Vb6covl05MUWlqZdHI2w",
+      sourceUrl: "https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k",
       mediaType: 1,
       renderLargerThumbnail: true,
       showAdAttribution: true
